232262a48bc5e566336516af537a09bf870bec8f
iregina <reginawong14@gmail.com>
2016-05-03 19:21:48 -0400

    add user information into form creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,23 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		  @article = Article.new(params[:article])
- 
-  		  @article.save
-          redirect_to @article
+		@user = current_user
+	  	@post = Post.new post_params
+	  	@post.author_id = @user.id
+	  	@post.save
+      	redirect_to @post
   	end
+
+  	def show
+  		@post = Post.find(params[:id])
+  	end
+
+  	private 
+
+  	   private
+
+   def post_params
+      params.require(:post).permit(:description)
+   end
+
 end
