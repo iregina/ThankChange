d39731472ace493e1333e471ba616ca5cbad20df
iregina <reginawong14@gmail.com>
2016-05-03 13:08:20 -0400

    allow users to save first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503162305) do
+ActiveRecord::Schema.define(version: 20160503165246) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "description"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160503162305) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
