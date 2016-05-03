65a626c1fa3af61fc2189a587e26056a6e13031e
iregina <reginawong14@gmail.com>
2016-05-03 18:09:37 -0400

    create posts folder in views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+	def new
+	end
+
+	def create
+		  @article = Article.new(params[:article])
+ 
+  		  @article.save
+          redirect_to @article
+  	end
 end
